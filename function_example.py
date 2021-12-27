fd3e51a3a315e537a924d4bf374b3475b13775c6
Fiona <96703368+fiona-dahlia@users.noreply.github.com>
2021-12-27 00:38:46 +0000

    examples

---
+++ function_example.py
@@ -0,0 +1,6 @@
+def add(addend1, addend2, addend3):
+  print('adding', addend1,  ',' , addend2,'and', addend3)
+  return addend1+ addend2+ addend3;
+
+a = add(1, 9, 5)
+print(a)
---
+++ operators_example.py
@@ -0,0 +1,50 @@
+# Arithmetic Operators
+x = 2
+y = x + 5
+print ("2 + 5 =", y)
+
+a = 20
+b = a - 15
+print ("20 - 15 =", b)
+
+f = 10
+j = f * 3
+print ("10 * 3 =", j)
+
+k = f / 5
+print ("10 / 5 =", k)
+
+g = 10 % 7
+print('remainder of 10 / 7 is', g)
+
+# Comparison Operators
+m = 7 != 7 # != is the same as <>
+print("7 is not equal to 7 is", m)
+
+o = 6 < 7
+print ('6 < 7 is', o)
+
+p = 9 <= 10
+print ("9 is less than or equal to 10 is", p)
+
+w = 15 >= 18
+print ("9 is more than or equal to 10 is", w)
+
+r = 8 == 9
+print ("8 is = to 9 is", r)
+
+# Assignment Operators
+t = a + k
+print ("20 plus 2 is", t)
+
+v = 5
+z = 7
+
+v += z
+print ("v + z is", v)
+
+z -= v
+print ("z - v is", z)
+
+v *= z
+print ("v *", v)
