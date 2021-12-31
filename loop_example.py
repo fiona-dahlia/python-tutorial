7aed9cbf635b91f9170bdca35dc2d4fd6cbeef0c
Fiona <96703368+fiona-dahlia@users.noreply.github.com>
2021-12-31 01:10:07 +0000

    Added loop examples.

---
+++ loop_example.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6]
+
+sum = 0
+
+for number in numbers:
+  sum = sum + number
+  print('element: ', number, ', sum: ', sum)
+
+print(sum)
+
+answer = 1
+for number in numbers:
+  answer = answer * number
+  print('element: ', number, ', answer =', answer)
+print(answer)
+
+even_sum = 0
+
+for number in numbers:
+  if number % 2 == 0:
+    even_sum = even_sum + number
+print(even_sum)
