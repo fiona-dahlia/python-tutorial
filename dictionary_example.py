91ea09d25e43993eea697a7e498fa87b6ab0da34
Fiona <96703368+fiona-dahlia@users.noreply.github.com>
2021-12-31 00:22:24 +0000

    Added dictionary examples.

---
+++ dictionary_example.py
@@ -0,0 +1,17 @@
+dict = {
+  'Remote' : 10,
+  'Number' : 19
+}
+
+print (dict)
+print (len(dict))
+print (dict.keys())
+print (dict.values())
+
+print (dict['Remote'])
+
+dict['Tree'] = 'Leaves'
+print(dict)
+
+dict['Number'] = 20
+print(dict)
